6191fec492218957e67d841c7e7ca2e48e9188de
teatov <hlebpoimenigleb@gmail.com>
2024-11-13 19:29:08 +1000

    build(package.json,-bootstrap.js): добавлен пакет Alpine.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,9 @@
-import '@fontsource-variable/inter';
-import axios from 'axios';
-window.axios = axios;
+import "@fontsource-variable/inter";
+import axios from "axios";
+import Alpine from "alpinejs";
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios = axios;
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+window.Alpine = Alpine;
+Alpine.start();
