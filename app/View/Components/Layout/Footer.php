afec92f76026ea48fee7458cc7a5a97e3c3a757f
teatov <hlebpoimenigleb@gmail.com>
2024-11-13 20:59:07 +1000

    feat(layout/footer): добавлен список ссылок для подвала

--- app/View/Components/Layout/Footer.php
+++ app/View/Components/Layout/Footer.php
@@ -1,25 +1,43 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\View\Components\Layout;
 
-use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class Footer extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
-    {
-        //
-    }
+    public array $navLinks = [
+        'О нас' => [
+            [
+                'Об институте' => '/about',
+                'История' => '/history',
+                'Сотрудники' => '/staff',
+            ],
+            [
+                'Новости' => '/news',
+                'Мероприятия' => '/events',
+                'Достижения' => '/achievements',
+            ],
+        ],
+        'Организации' => [
+            [
+                'Организация 1' => '/1',
+                'Организация 2' => '/2',
+                'Организация 3' => '/3',
+                'Организация 4' => '/4',
+            ],
+            [
+                'Диссертационный пакет' => '/dissertations',
+                'Список патентов' => '/patents',
+                'Оборудование' => '/equipment',
+            ],
+        ],
+    ];
 
-    /**
-     * Get the view / contents that represent the component.
-     */
-    public function render(): View|Closure|string
+    public function render(): View
     {
         return view('components.layout.footer');
     }
--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,10 +1,26 @@
 <footer class="bg-foreground text-neutral-darker">
-    <div class="max-w-screen-1.5xl container mx-auto px-15 py-14">
-        <nav>
-            <ul class="flex gap-x-4">
-                <li><x-ui.link href="/1">О нас</x-ui.link></li>
-                <li><x-ui.link href="/2">Направления</x-ui.link></li>
-            </ul>
+    <div
+        class="max-w-screen-1.5xl container mx-auto flex flex-col justify-between gap-y-6 px-4 py-14 md:flex-row md:px-15">
+        <nav class="space-y-9 md:min-w-[50%]">
+            @foreach ($navLinks as $sectionLabel => $sectionColumns)
+                <div>
+                    <h6 class="mb-4 text-2xl font-semibold text-background">{{ $sectionLabel }}</h6>
+                    <ul class="flex w-full flex-col gap-x-4 space-y-4 sm:flex-row sm:space-y-0">
+                        @foreach ($sectionColumns as $column)
+                            <div class="w-full space-y-0.5">
+                                @foreach ($column as $label => $navLink)
+                                    <li><x-ui.link href="{{ $navLink }}">{{ $label }}</x-ui.link>
+                                    </li>
+                                @endforeach
+                            </div>
+                        @endforeach
+                    </ul>
+                </div>
+            @endforeach
         </nav>
+        <div class="flex flex-col items-start justify-between space-y-4 md:items-end">
+            <x-ui.button href="/">Связаться с нами</x-ui.button>
+            <x-ui.link href="/privacy">Политика конфиденциальности</x-ui.link>
+        </div>
     </div>
 </footer>
