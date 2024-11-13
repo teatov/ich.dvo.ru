b6964e0bb06ff5e21994ef079fa893784ef07802
teatov <hlebpoimenigleb@gmail.com>
2024-11-13 22:34:05 +1000

    feat(ui/dropdown): изменено назначение классов к слотам

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -5,26 +5,25 @@
             <ul class="flex gap-x-4">
                 @foreach ($navLinks as $label => $navLink)
                     @if (is_array($navLink) && !array_is_list($navLink))
-                        <x-ui.dropdown wrapperClasses="mt-6" contentClasses="flex flex-col divide-y">
-                            <x-slot name="trigger">{{ $label }}</x-slot>
-                            <x-slot name="content">
+                        <x-ui.dropdown wrapperClasses="mt-6">
+                            <x-slot:trigger>{{ $label }}</x-slot:trigger>
+                            <x-slot:content class="flex flex-col divide-y">
                                 @foreach ($navLink as $subLabel => $subNavLink)
                                     <li><x-ui.link href="{{ $subNavLink }}"
-                                            class="block mx-5 my-3">{{ $subLabel }}</x-ui.link></li>
+                                            class="mx-5 my-3 block">{{ $subLabel }}</x-ui.link></li>
                                 @endforeach
-                            </x-slot>
+                            </x-slot:content>
                         </x-ui.dropdown>
                     @elseif (is_array($navLink) && array_is_list($navLink))
                         <x-ui.dropdown
-                            wrapperClasses="mt-6 left-0 right-0 max-w-screen-1.5xl container mx-auto px-4 md:px-15"
-                            contentClasses="columns-4 space-y-6 px-24 py-16">
-                            <x-slot name="trigger">{{ $label }}</x-slot>
-                            <x-slot name="content">
+                            wrapperClasses="mt-6 left-0 right-0 max-w-screen-1.5xl container mx-auto px-4 lg:px-15">
+                            <x-slot:trigger>{{ $label }}</x-slot:trigger>
+                            <x-slot:content class="columns-4 space-y-6 px-24 py-16">
                                 @foreach ($navLink[0] as $subLabel => $subNavLink)
                                     <li class="text-center"><x-ui.link
                                             href="{{ $subNavLink }}">{{ $subLabel }}</x-ui.link></li>
                                 @endforeach
-                            </x-slot>
+                            </x-slot:content>
                         </x-ui.dropdown>
                     @else
                         <li><x-ui.link href="{{ $navLink }}">{{ $label }}</x-ui.link></li>
--- resources/views/components/ui/dropdown.blade.php
+++ resources/views/components/ui/dropdown.blade.php
@@ -1,19 +1,22 @@
-@props(['wrapperClasses' => '', 'contentClasses' => ''])
+@props(['wrapperClasses' => '', 'chevron' => true])
 
 <div x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
-    <button class="flex items-center gap-x-1.5 hover:text-primary active:text-primary-darker" @click="open = ! open">
+    <button class="flex items-center gap-x-1.5 font-medium hover:text-primary active:text-primary-darker"
+        @click="open = ! open">
         {{ $trigger }}
-        <svg width="8" height="4" viewBox="0 0 8 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M4.10766 4L7.57176 0H0.643555L4.10766 4Z" fill="currentColor" />
-        </svg>
+        @if ($chevron)
+            <svg width="8" height="4" viewBox="0 0 8 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4.10766 4L7.57176 0H0.643555L4.10766 4Z" fill="currentColor" />
+            </svg>
+        @endif
     </button>
 
-    <div x-show="open" class="{{ $wrapperClasses }} absolute z-50 bg-background" style="display: none;"
+    <div x-show="open" class="{{ $wrapperClasses }} absolute z-50 origin-top bg-background" style="display: none;"
         x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 scale-95"
         x-transition:enter-end="opacity-100 scale-100" x-transition:leave="transition ease-in duration-75"
         x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95"
         @click="open = false">
-        <div class="text-nowrap {{ $contentClasses }} border text-sm">
+        <div {{ $content->attributes->class('text-nowrap border text-sm') }}>
             {{ $content }}
         </div>
     </div>
