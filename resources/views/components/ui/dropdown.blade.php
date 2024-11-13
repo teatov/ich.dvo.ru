c009c7034db95190df10abd6241d02df32826604
teatov <hlebpoimenigleb@gmail.com>
2024-11-13 23:36:30 +1000

    fix(dropdown): исправлен цвет фона

--- resources/views/components/ui/dropdown.blade.php
+++ resources/views/components/ui/dropdown.blade.php
@@ -11,12 +11,12 @@
         @endif
     </button>
 
-    <div x-show="open" class="{{ $wrapperClasses }} absolute z-50 origin-top bg-background" style="display: none;"
+    <div x-show="open" class="{{ $wrapperClasses }} absolute z-50 origin-top" style="display: none;"
         x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 scale-95"
         x-transition:enter-end="opacity-100 scale-100" x-transition:leave="transition ease-in duration-75"
         x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95"
         @click="open = false">
-        <div {{ $content->attributes->class('text-nowrap border text-sm') }}>
+        <div {{ $content->attributes->class('text-nowrap border text-sm bg-background') }}>
             {{ $content }}
         </div>
     </div>
