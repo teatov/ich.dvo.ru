c62895a52e54a99af30fba032b28abccc5b03c0c
Teatov <hlebpoimenigleb@gmail.com>
2024-11-13 14:46:20 +1000

    feat(layouts): добавлен лейаут, основанный на компоненте

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,21 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ $title }}</title>
 
-    <title>@yield('title')</title>
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    @vite(['resources/css/app.css', 'resources/css/reset.css', 'resources/css/header-nav.css', 'resources/js/app.js'])
-</head>
-
-<body>
-    @yield('content')
-</body>
-
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+    </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,8 @@
-@extends('layouts.app')
-@section('title', 'Главная страница')
-@section('content')
+<x-app-layout>
+    <x-slot:title>
+        Институт химии ДВО РАН
+    </x-slot>
 
-@endsection
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti provident animi explicabo nobis alias et
+        expedita minus rem nihil molestiae.</p>
+</x-app-layout>
