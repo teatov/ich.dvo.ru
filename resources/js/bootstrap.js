da38b42e66b41828aa1cdd91e487c4b3ae2f487c
Teatov <hlebpoimenigleb@gmail.com>
2024-11-13 14:55:12 +1000

    feat(package.json,-resources/,-tailwind.config.js): добавлен шрифт Inter

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,4 @@
+import '@fontsource-variable/inter';
 import axios from 'axios';
 window.axios = axios;
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,6 @@
 
         <title>{{ $title }}</title>
 
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
