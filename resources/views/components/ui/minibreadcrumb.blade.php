25a4a9ca1ca18da8b9fb6c7ee438763bfc30f360
Danila Belyak <belakdanila9@gmail.com>
2024-11-15 01:11:14 +1000

    feat(minibreadcrumb.blade.php): Добавлен новый компонент
    BREAKING CHANGE:

---
+++ resources/views/components/ui/minibreadcrumb.blade.php
@@ -0,0 +1,3 @@
+<div class="flex justify-start mb-6">
+    <span {{ $attributes->twMerge(['class' => 'font-semibold inline-block rounded bg-neutral-lighter px-3 py-2 text-xs uppercase text-neutral-darker']) }}>{{ $slot }}</span>
+</div>
