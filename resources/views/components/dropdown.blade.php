ea8cc8712318e00721503c3503e0f8d3d1a9042a
teatov <hlebpoimenigleb@gmail.com>
2024-11-15 19:07:10 +1000

    feat(collapsible,-dropdown,-header): упрощено добавление классов к врапперу `dropdown`, правки стилей

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,19 +1,17 @@
-@props(['wrapperClasses' => '', 'chevron' => true])
+@props(['chevron' => true])
 
 <div x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
     <button class="flex items-center gap-x-1.5 font-medium hover:text-primary active:text-primary-darker"
         @click="open = ! open">
         {{ $trigger }}
         @if ($chevron)
-            <svg width="8" height="4" viewBox="0 0 8 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M4.10766 4L7.57176 0H0.643555L4.10766 4Z" fill="currentColor" />
-            </svg>
+            <x-icon.chevron />
         @endif
     </button>
 
-    <div x-show="open" class="{{ $wrapperClasses }} absolute z-50 origin-top" style="display: none;"
-        x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 scale-95"
-        x-transition:enter-end="opacity-100 scale-100" x-transition:leave="transition ease-in duration-75"
+    <div {{ $wrapper->attributes->class('absolute z-50 origin-top transition') }} x-show="open" style="display: none;"
+        x-transition:enter="ease-out duration-200" x-transition:enter-start="opacity-0 scale-95"
+        x-transition:enter-end="opacity-100 scale-100" x-transition:leave="ease-in duration-75"
         x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95"
         @click="open = false">
         <div {{ $content->attributes->class('text-nowrap border text-sm bg-background') }}>
--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -1,5 +1,4 @@
-<header class="container sticky left-0 right-0 top-0 bg-background"
-    x-data="{ headerOpen: false }">
+<header class="container sticky left-0 right-0 top-0 bg-background" x-data="{ headerOpen: false }">
     <div class="flex items-center justify-between border-b py-3">
         <x-link href="/" class="block text-sm font-bold tracking-wider">ИНСТИТУТ ХИМИИ<br />ДВО РАН</x-link>
 
@@ -7,25 +6,28 @@
             <ul class="flex gap-x-4">
                 @foreach ($navLinks as $label => $navLink)
                     @if (is_array($navLink) && !array_is_list($navLink))
-                        <x-dropdown wrapperClasses="mt-6">
+                        <x-dropdown>
                             <x-slot:trigger>{{ $label }}</x-slot:trigger>
-                            <x-slot:content class="flex flex-col divide-y">
-                                @foreach ($navLink as $subLabel => $subNavLink)
-                                    <li><x-link href="{{ $subNavLink }}"
-                                            class="mx-5 my-3 block">{{ $subLabel }}</x-link></li>
-                                @endforeach
-                            </x-slot:content>
+                            <x-slot:wrapper class="mt-[1.4rem]">
+                                <x-slot:content class="flex flex-col divide-y">
+                                    @foreach ($navLink as $subLabel => $subNavLink)
+                                        <li><x-link href="{{ $subNavLink }}"
+                                                class="mx-5 my-3 block">{{ $subLabel }}</x-link></li>
+                                    @endforeach
+                                </x-slot:content>
+                            </x-slot:wrapper>
                         </x-dropdown>
                     @elseif (is_array($navLink) && array_is_list($navLink))
-                        <x-dropdown
-                            wrapperClasses="mt-6 left-0 right-0 container">
+                        <x-dropdown>
                             <x-slot:trigger>{{ $label }}</x-slot:trigger>
-                            <x-slot:content class="columns-4 space-y-6 px-24 py-16">
-                                @foreach ($navLink[0] as $subLabel => $subNavLink)
-                                    <li class="text-center"><x-link
-                                            href="{{ $subNavLink }}">{{ $subLabel }}</x-link></li>
-                                @endforeach
-                            </x-slot:content>
+                            <x-slot:wrapper class="container left-0 right-0 mt-[1.4rem]">
+                                <x-slot:content class="columns-4 space-y-6 px-24 py-16">
+                                    @foreach ($navLink[0] as $subLabel => $subNavLink)
+                                        <li class="text-center"><x-link
+                                                href="{{ $subNavLink }}">{{ $subLabel }}</x-link></li>
+                                    @endforeach
+                                </x-slot:content>
+                            </x-slot:wrapper>
                         </x-dropdown>
                     @else
                         <li><x-link href="{{ $navLink }}">{{ $label }}</x-link></li>
