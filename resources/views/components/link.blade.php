c4916e5cd3b956d5d6008b101d9add5f7760700a
teatov <hlebpoimenigleb@gmail.com>
2024-11-15 19:07:52 +1000

    feat(components/link): добавлены варианты к ссылке

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,1 +1,11 @@
-<a {{ $attributes->twMerge(['class' => 'font-medium hover:text-primary active:text-primary-darker']) }}>{{ $slot }}</a>
+@props(['variant' => 'default'])
+
+@php
+    $classes = [
+        'default' => 'font-medium hover:text-primary active:text-primary-darker',
+        'primary' =>
+            'font-semibold underline underline-offset-4 decoration-primary/20 text-primary decoration-2 hover:text-primary-lighter hover:decoration-primary-lighter active:text-primary-darker active:decoration-primary-darker',
+    ];
+@endphp
+
+<a {{ $attributes->twMerge(['class' => $classes[$variant]]) }}>{{ $slot }}</a>
