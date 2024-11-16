88c13d55a93d0fb1ab95f92c51e929dd6e9d59a2
Danila Belyak <belakdanila9@gmail.com>
2024-11-16 10:22:02 +1000

    feat(views/about): Добавлена вёрстка
    BREAKING CHANGE:

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,9 +3,52 @@
         Об институте
     </x-slot>
 
-    <x-layout.breadcrumb>Институт</x-layout.breadcrumb>
+    <section class="">
+        <x-layout.breadcrumb>Институт</x-layout.breadcrumb>
+        <x-h1>Об институте</x-h1>
+        <x-lead>Lorem ipsum dolor sit amet, consectetur adipiscing elit</x-lead>
 
-    <x-h1>Об институте</x-h1>
-
-    <x-lead>Lorem ipsum dolor sit amet, consectetur adipiscing elit</x-lead>
+        <div class="py-36 bg-[#d6d6d6] text-center content-center mb-16">
+            Image
+        </div>
+        <div class="max-w-[880px] mx-auto pb-16 text-[#606567] text-xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+    </section>
+    <div class="border border-solid border-neutral"></div>
+    <section class="mt-16 mb-16">
+        <div class="">
+            <ul class="flex justify-between">
+                <li class="max-w-[338px]">
+                    <img class='mb-7' src="" alt="image">
+                    <h3 class="font-semibold text-2xl mb-5">Информация</h3>
+                    <p class="text-[#606567]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                </li>
+                <li class="max-w-[338px]">
+                    <img class='mb-7' src="" alt="image">
+                    <h3 class="font-semibold text-2xl mb-5">Информация</h3>
+                    <p class="text-[#606567]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                </li>
+                <li class="max-w-[338px]">
+                    <img class='mb-7' src="" alt="image">
+                    <h3 class="font-semibold text-2xl mb-5">Информация</h3>
+                    <p class="text-[#606567]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <section class='mt-16'>
+        <div class='flex justify-between'>
+            <div class="max-w-[532px]">
+                <h2 class='font-bold text-5xl mb-5'>Заголовок</h2>
+                <p class="text-[#606567] leading-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    <br><br>
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <br><br>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            </div>
+            <div class="py-60 px-80 bg-[#d6d6d6]">Image</div>
+        </div>
+    </section>
 </x-app-layout>
