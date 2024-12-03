e5e7a04bf2e9a0f3fe64911a515fe4fa5b51c09b
teatov <hlebpoimenigleb@gmail.com>
2024-12-03 10:42:30 +1000

    refactor(views/about): проведён рефакторинг

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,74 +3,72 @@
         Об институте
     </x-slot>
 
-    <section class="">
+    <section class="mb-16">
         <x-layout.breadcrumb>Институт</x-layout.breadcrumb>
         <x-h1>Об институте</x-h1>
         <x-lead>Lorem ipsum dolor sit amet, consectetur adipiscing elit</x-lead>
+        <x-img class="mb-16 max-h-80 rounded"
+            src="https://images.unsplash.com/photo-1518152006812-edab29b069ac?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        <x-lead>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat.</x-lead>
+    </section>
 
-        <div class="py-36 bg-[#d6d6d6] text-center content-center mb-16">
-            Image
+    <hr />
+
+    <section class="my-16">
+        <ul class="flex justify-between gap-16">
+            @foreach (['1', '2', '3'] as $info)
+                <li class="space-y-5">
+                    <x-icon.placeholder />
+                    <x-h4>Lorem ipsum dolor sit amet {{ $info }}</x-h4>
+                    <x-p class="text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</x-p>
+                </li>
+            @endforeach
+        </ul>
+    </section>
+
+    <hr />
+
+    <section class='my-16 flex'>
+        <div class="w-1/2 py-10 pr-28">
+            <x-h2>Заголовок</x-h2>
+            <x-p class="text-base">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat.
+                <br><br>
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                anim id est laborum.
+                <br><br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat.
+            </x-p>
         </div>
-        <div class="max-w-[880px] mx-auto pb-16 text-[#606567] text-xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <div class="w-1/2">
+            <x-img class="rounded"
+                src="https://images.unsplash.com/photo-1518152006812-edab29b069ac?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         </div>
     </section>
-    <div class="border border-solid border-neutral"></div>
-    <section class="mt-16 mb-16">
-        <div class="">
-            <ul class="flex justify-between">
-                <li class="max-w-[338px]">
-                    <img class='mb-7' src="" alt="image">
-                    <h3 class="font-semibold text-2xl mb-5">Информация</h3>
-                    <p class="text-[#606567]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                </li>
-                <li class="max-w-[338px]">
-                    <img class='mb-7' src="" alt="image">
-                    <h3 class="font-semibold text-2xl mb-5">Информация</h3>
-                    <p class="text-[#606567]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                </li>
-                <li class="max-w-[338px]">
-                    <img class='mb-7' src="" alt="image">
-                    <h3 class="font-semibold text-2xl mb-5">Информация</h3>
-                    <p class="text-[#606567]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                </li>
-            </ul>
+
+    <section class='mt-16 flex'>
+        <div class="w-1/2 py-10 pr-28">
+            <x-h2>Наши партнёры</x-h2>
+            <x-p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam,
+            </x-p>
         </div>
-    </section>
-    <div class="border border-solid border-neutral"></div>
-    <section class='mt-16 mb-24'>
-        <div class='flex justify-between items-center'>
-            <div class="max-w-[532px]">
-                <h3 class='font-bold text-5xl mb-5'>Заголовок</h3>
-                <p class="text-[#606567] leading-5">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    <br><br>
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    <br><br>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-            </div>
-            <div class="py-60 px-80 bg-[#d6d6d6]">Image</div>
-        </div>
-    </section>
-    <section class="">
-        <div class=" flex items-center gap-x-16">
-            <div class="max-w-[532px]">
-                <h3 class="font-bold text-5xl mb-5 ">Наши партнёры</h3>
-                <p class="text-[#606567] leading-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
-            </div>
-            <div class="w-full">
-                <ul class="flex max-w-[548px] flex-wrap gap-x-8 gap-y-8 mx-auto">
-                    <li class="px-15 pt-4 pb-4 bg-neutral-lighter items-center text-center text-xl"><a href="#!">logo</a></li>
-                    <li class="px-15 pt-4 pb-4 bg-neutral-lighter items-center text-center text-xl"><a href="#!">logo</a></li>
-                    <li class="px-15 pt-4 pb-4 bg-neutral-lighter items-center text-center text-xl"><a href="#!">logo</a></li>
-                    <li class="px-15 pt-4 pb-4 bg-neutral-lighter items-center text-center text-xl"><a href="#!">logo</a></li>
-                    <li class="px-15 pt-4 pb-4 bg-neutral-lighter items-center text-center text-xl"><a href="#!">logo</a></li>
-                    <li class="px-15 pt-4 pb-4 bg-neutral-lighter items-center text-center text-xl"><a href="#!">logo</a></li>
-                    <li class="px-15 pt-4 pb-4 bg-neutral-lighter items-center text-center text-xl"><a href="#!">logo</a></li>
-                    <li class="px-15 pt-4 pb-4 bg-neutral-lighter items-center text-center text-xl"><a href="#!">logo</a></li>
-                    <li class="px-15 pt-4 pb-4 bg-neutral-lighter items-center text-center text-xl"><a href="#!">logo</a></li>
-                </ul>
-            </div>
-        </div>
+        <ul class="grid w-1/2 grid-cols-3 gap-9">
+            @foreach (['1', '2', '3', '4', '5', '6', '7', '8', '9'] as $partner)
+                <li class="flex items-center justify-center rounded bg-neutral-lighter text-neutral-darker">
+                    {{ $partner }}
+                </li>
+            @endforeach
+        </ul>
     </section>
 </x-app-layout>
--- resources/views/components/lead.blade.php
+++ resources/views/components/lead.blade.php
@@ -1,1 +1,1 @@
-<p {{ $attributes->twMerge(['class' => 'my-4 text-xl text-center text-neutral-darker']) }}>{{ $slot }}</p>
+<p {{ $attributes->twMerge(['class' => 'max-w-screen-md mx-auto my-4 text-xl text-center text-neutral-darker']) }}>{{ $slot }}</p>
