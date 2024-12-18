b25d3fe86ee83de454b1e06e77c2ef04971c2190
Belyak-Danila <108313791+Danila506@users.noreply.github.com>
2024-12-18 15:02:09 +1000

    feat(views/themes,-grants): Реализована верстка под десктоп
    BREAKING CHANGE:

--- resources/views/grants.blade.php
+++ resources/views/grants.blade.php
@@ -3,5 +3,61 @@
         Гранты
     </x-slot>
 
-    <p>Гранты</p>
+    <section class="">
+        <x-layout.breadcrumb>Гранты</x-layout.breadcrumb>
+        <x-h1 class="mb-8">Гранты</x-h1>
+        <hr>
+        <div class="flex items-center gap-2 justify-end mt-4 mb-8">
+            <div class="relative inline-block text-[#666]">
+                <select class="appearance-none bg-white border rounded pr-24 py-2 pl-5">
+                    <option>Выбрать год</option>
+                    <option value="2024">2025</option>
+                    <option value="2023">2024</option>
+                    <option value="2022">2023</option>
+                    <option value="2021">2022</option>
+                    <option value="2020">2021</option>
+                    <option value="2021">2020</option>
+                    <option value="2020">2019</option>
+                </select>
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-4 ">
+                    <svg width="14" height="7" viewBox="0 0 14 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12.6567 0.5L6.99988 6.15685L1.34303 0.5" stroke="#666666"/>
+                    </svg>
+                </div>
+            </div>
+            <div class="relative inline-block text-[#666]">
+            <select class="appearance-none bg-white border rounded pr-12 py-2 pl-5">
+                <option>Выбрать категорию</option>
+                <option value="technology">Технологии</option>
+               <option value="science">Наука</option>
+              <option value="art">Искусство</option>
+               <option value="business">Бизнес</option>
+                 <option value="sport">Спорт</option>
+           </select>
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-4 ">
+                    <svg width="14" height="7" viewBox="0 0 14 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12.6567 0.5L6.99988 6.15685L1.34303 0.5" stroke="#666666"/>
+                    </svg>
+                </div>
+            </div>
+        </div>
+        <ul class="flex flex-wrap justify-between gap-7 mb-10">
+            @for ($i = 0; $i < 9; $i++)
+                <li class="px-5 pt-5 pb-9 border border-neutral w-[407px] rounded-[5px]">
+                    <x-img class="h-auto rounded mb-5"
+                        src="https://images.unsplash.com/photo-1562411053-c9ac630a5934?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                    <div class="ml-4">
+                        <div class="flex justify-start mb-3">
+                            <span class="font-semibold inline-block rounded bg-neutral-lighter px-3 py-2 text-xs uppercase text-neutral-darker">Категория</span>
+                        </div>
+                        <x-h4 class="sm:text-xl mb-5 max-w-[320px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</x-h4>
+                        <x-p class="text-base mb-6 mt-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</x-p>
+                        <x-link class="text-[#242627]">Подробнее</x-link>
+                    </div>
+
+                </li>
+            @endfor
+        </ul>
+        <x-link href="{{ route('about') }}" variant="primary" class="text-sm font-semibold">Показать ещё</x-link>
+    </section>
 </x-app-layout>
--- resources/views/themes.blade.php
+++ resources/views/themes.blade.php
@@ -2,6 +2,63 @@
     <x-slot:title>
         Тематики
     </x-slot>
-
-    <p>Тематики</p>
+    <section class="mb-16">
+        <x-layout.breadcrumb>Тематики</x-layout.breadcrumb>
+        <x-h1>Тематики</x-h1>
+        <x-lead>Lorem ipsum dolor sit amet, consectetur adipiscing elit</x-lead>
+        <x-img class="mb-16 max-h-80 rounded"
+            src="https://images.unsplash.com/photo-1518152006812-edab29b069ac?q=80&w=207
+            0&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            <x-lead class="max-w-[900px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                 ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                  incididunt ut labore et dolore magna aliqua. Ut enim </x-lead>
+    </section>
+    <section class="pt-14 bg-neutral-lighter pb-11 my-16">
+        <h2 class="text-5xl font-semibold mb-8">Тематики 2025</h2>
+        <ul class="flex justify-between">
+            @foreach (['1', '2', '3', '4'] as $info)
+            <div class="bg-background pb-5 rounded-[5px]">
+                <li class=" w-[295px] px-9 pt-9">
+                    <x-h4>Lorem ipsum dolor sit amet</x-h4>
+                    <x-p class="text-base mb-12 mt-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</x-p>
+                </li>
+                <hr>
+                <x-link href="#!" class="block pl-9 mt-4">Подробнее</x-link>
+            </div>
+            @endforeach
+        </ul>
+    </section>
+    <section class="bg-neutral-lighter pb-16 pt-8 px-16 mb-16">
+        <ul class="flex flex-col">
+            @foreach (['2025', '2024', '2023', '2022', '2021', '2020', '2019'] as $info)
+                <li class="flex justify-between pt-8 pb-6">
+                    <x-h4 class="my-0 py-0 sm:text-3xl">{{$info}}</x-h4>
+                    <button class="flex gap-x-2 items-center pb-5">
+                        <div class="w-7 h-7 bg-[#606568] rounded-full flex justify-center items-center">
+                            <svg class="w-[7px] h-[7px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 5" fill="none">
+                                <path d="M2.66699 0.5V4.5" stroke="white"/>
+                                <path d="M4.66699 2.5L0.666992 2.5" stroke="white"/>
+                            </svg>
+                        </div>
+                    </button>
+                </li>
+                <hr>
+            @endforeach
+        </ul>
+    </section>
+    <section class="flex items-center justify-between">
+        <div class="w-2/5 max-w-[425px]">
+            <div class="">
+                <h2 class="text-5xl leading-[58px] font-bold mb-5">О нашем институте</h2>
+                <p class="mb-8 text-lg leading-[25px] text-[#606567]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    <br><br>
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
+                    <x-button href="/">Подробнее</x-button>
+            </div>
+        </div>
+        <div class="w-3/5">
+            <img class="max-h-[424px] w-full float-end rounded-[5px] object-cover" src="https://images.unsplash.com/photo-1518152006812-edab29b069ac?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+        </div>
+    </section>
 </x-app-layout>
