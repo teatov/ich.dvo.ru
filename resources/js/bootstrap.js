0b3752cb452edf976b3d79e7433d592ad94a9144
Teatov <hlebpoimenigleb@gmail.com>
2025-06-16 23:43:37 +1000

    build(alpinejs): добавлен `@alpinejs/focus`

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,10 +1,12 @@
 import "@fontsource-variable/inter";
 import axios from "axios";
 import Alpine from "alpinejs";
-import collapse from '@alpinejs/collapse'
+import collapse from "@alpinejs/collapse";
+import focus from "@alpinejs/focus";
 
 window.axios = axios;
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-Alpine.plugin(collapse)
+Alpine.plugin(collapse);
+Alpine.plugin(focus);
 window.Alpine = Alpine;
