90cac606181625f465f2fa8081d3206ed2804fe9
teatov <hlebpoimenigleb@gmail.com>
2026-01-19 09:00:59 +1000

    refactor(admin): убраны контроллеры и маршруты

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\EditTextController;
-use Illuminate\Support\Facades\Route;
-
-Route::name('api.')->group(function () {
-    Route::post('/edit-text', EditTextController::class)->name('edit-text');
-});
--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig({
             input: [
                 "resources/css/app.css",
                 "resources/js/app.js",
-                "resources/js/tiptap-editor.js",
             ],
             refresh: true,
         }),
