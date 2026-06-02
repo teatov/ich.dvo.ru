4e687ea983e50bafa7d20b9f85b168c9cb70db3f
teatov <hlebpoimenigleb@gmail.com>
2026-06-02 13:25:05 +1000

    fix(config): добавлен fallback_locale

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
 
     'locale' => env('APP_LOCALE', 'ru'),
 
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'ru'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'ru_RU'),
 
--- config/twill.php
+++ config/twill.php
@@ -2,6 +2,7 @@
 
 return [
     'locale' => 'ru',
+    'fallback_locale' => 'en',
     'block_editor' => [
         'use_twill_blocks' => [],
     ],
